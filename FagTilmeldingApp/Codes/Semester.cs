4de034a64f570a4ca931c6380aff6f7768eaf1d4
Jack Nguyen <Jaackie@live.dk>
2022-02-21 10:02:12 +0100

    a

---
+++ FagTilmeldingApp/Codes/Semester.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal class Semester
+    {
+        string? SemesterNavn { get; set; }
+
+        public Semester(string semesterNavn)
+        {
+            SemesterNavn = semesterNavn;
+        }
+
+    }
+}
