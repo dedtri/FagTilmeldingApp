1be59705409aec468902ee080b67d3b48fbf5504
Jack Nguyen <Jaackie@live.dk>
2022-02-21 09:51:24 +0100

    Add project files.

---
+++ FagTilmeldingApp/Codes/Bil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal class Bil : Motorkøretøj
+    {
+        public bool ErFirHjulTræk { get; set; }
+
+        public Bil(bool erFirHjulTræk, double motorStørrelse) : base(motorStørrelse)
+        {
+            ErFirHjulTræk = erFirHjulTræk;
+        }
+
+        public override void SetServiceInterval()
+        {
+        }
+
+        public string Test()
+        {
+            return "";
+        }
+
+        public void Test(string s)
+        {
+        }
+    }
+
+}
---
+++ FagTilmeldingApp/Codes/Motorkøretøj.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal abstract class Motorkøretøj
+    {
+        public double MotorStørrelse { get; set; }
+
+        public Motorkøretøj(double motorStørrelse)
+        {
+            MotorStørrelse = motorStørrelse;
+        }
+
+        public abstract void SetServiceInterval();
+    }
+
+}
