2a9a75bdf200505b1b7956a7c3bcc861e87a1338
Jack Nguyen <Jaackie@live.dk>
2022-02-22 12:28:46 +0100

    d

---
+++ FagTilmeldingApp/Codes/ADOHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FagTilmeldingApp.Codes
+{
+    internal class ADOHandler
+    {
+        public string? ConnectionString 
+        {
+            get => "Data Source=MARTIN-PC;Initial Catalog=TEC;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+    }
+}
