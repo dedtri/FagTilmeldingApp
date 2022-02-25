a916a979de79a96fa0b24888197cb2004e9b7018
Jack Nguyen <Jaackie@live.dk>
2022-02-25 09:43:15 +0100

    dasd

--- FagTilmeldingApp/Codes/Box.cs
+++ FagTilmeldingApp/Codes/Box.cs
@@ -6,7 +6,92 @@ using System.Threading.Tasks;
 
 namespace FagTilmeldingApp.Codes
 {
-    internal class Box
+    internal class Box : IComparable<Box>
     {
+        private double Højde { get; set; }
+        private double Bredde { get; set; }
+        private double Længde { get; set; }
+
+        public double Volume { get; set; }
+
+        private BoxType Type { get; set; }
+
+        public Box()
+        { }
+        public Box(double H, double B, double L)
+        {
+            Højde = H;
+            Bredde = B;
+            Længde = L;
+            GetVolume();
+            SetBoxType();
+        }
+
+        private void GetVolume()
+        {
+            Volume = Højde * Længde * Bredde;
+        }
+
+        public override string ToString()
+        {
+          return "Volume: " + Volume + ", Type: " + Type.ToString();
+        }
+
+        public static Box operator +(Box a, Box b)
+        {
+            Box box = new Box();
+
+            //box.Længde = b.Længde + a.Længde;
+            //box.Højde = b.Højde + a.Højde;
+            //box.Bredde = b.Bredde + a.Bredde;
+            //box.Volume = box.Bredde * box.Højde * box.Længde;
+
+            box.Volume = a.Volume + b.Volume;
+
+            //regel Lillebox + lillebox giver mellembox, resten er storBox.
+            if (a.Type == BoxType.lilleBox && b.Type == BoxType.lilleBox)
+            {
+                box.Type = BoxType.mellemBox;
+            }
+            else
+            {
+                box.Type = BoxType.storBox;
+            }
+
+            return box;
+        }
+
+        private void SetBoxType()
+        {
+
+            if(Volume <= 125)
+            {
+                Type = BoxType.lilleBox;
+            }
+            else if(Volume >= 126 && Volume <= 1500)
+            {
+                Type = BoxType.mellemBox;
+            }
+            else
+            {
+                Type = BoxType.storBox;
+            }
+
+
+            //? = If
+            //: = Else/Else if
+            //Type = (Volume <= 125) ? BoxType.lilleBox : (Volume >= 126 && Volume <= 1500) ? BoxType.mellemBox : BoxType.storBox;
+        }
+
+        public int CompareTo(Box next)
+        {
+            return this.Volume.CompareTo(next.Volume);
+        }
+
+        //public int CompareTo(Box next)
+        //{
+
+        //}
+
     }
 }
--- FagTilmeldingApp/Program.cs
+++ FagTilmeldingApp/Program.cs
@@ -1,4 +1,24 @@
-﻿using FagTilmeldingApp.Codes;
+﻿//Iteration 9
 
-//Iteration 9
+Box a = new Box(9.0, 5.0, 5.0);
+Console.WriteLine("Box A: " + a.ToString());
 
+Console.WriteLine();
+
+Box b = new Box(9.0, 4.0, 4.0);
+Console.WriteLine("Box B: " + b.ToString());
+
+Console.WriteLine();
+
+Box c = a + b;
+Console.WriteLine("Box C: " + c.ToString());
+
+List<Box> boxes = new List<Box> { a, b, c };
+
+boxes.Sort();
+boxes.Reverse();
+
+foreach (Box d in boxes)
+{
+    Console.WriteLine("Volume: " + d.Volume);
+}
